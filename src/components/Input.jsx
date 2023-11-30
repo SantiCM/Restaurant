ce3e231f88c3e0809e5de2afe1955b6860fa368a
Santiago Melo <santiagocano15cr7@gmail.com>
2023-11-29 21:26:49 -0600

    Comments

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,18 @@
 
-
+// Componente del Input
+// le damos el label para poner como se llama ese campo
+// el id y la copia de las props
 export const Input = ( { label, id, ...props }) => {
   
     return (
     
         <p className="control">
 
+            { /* Damos el htmlFor del id y el label */}
+
             <label htmlFor={id}>{label}</label>
+
+            { /* Damos el id y el name tambien como id, el required y la copia de las props */}
 
             <input id={id} name={id} required {...props}></input>
 
